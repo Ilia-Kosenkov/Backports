76b5d97801ef35d4eb74e3f72366fea9e55a9bf9
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2020-10-19 03:06:58 +0300

    Fixed NumberToFloatngPointBIts

--- FormatPorts/System/BitConverter.cs
+++ FormatPorts/System/BitConverter.cs
@@ -6,5 +6,17 @@ namespace Backports.System
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float Int32BitsToSingle(int value) => Unsafe.As<int, float>(ref value);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int SingleToInt32Bits(float value) => Unsafe.As<float, int>(ref value);
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Half Int16BitsToHalf(short value) => Unsafe.As<short, Half>(ref value);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static short HalfToInt16Bits(Half value) => Unsafe.As<Half, short>(ref value);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static long DoubleToInt64Bits(double value) => Unsafe.As<double, long>(ref value);
     }
 }
