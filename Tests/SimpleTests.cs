abac35bcd09f8bea7b5bcd19ae9f7b6bd38f7701
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2020-10-19 21:58:26 +0300

    Added tests for all cases

--- Tests/SimpleTests.cs
+++ Tests/SimpleTests.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 
 namespace Tests
 {
-    public class SimpleTests
+    public class SanityChecks
     {
         [SetUp]
         public void Setup()
@@ -13,13 +13,85 @@ namespace Tests
         }
 
         [Test]
+        public void Test_TryParseInt8()
+        {
+            sbyte numRaw = 123;
+            var num = numRaw.ToString();
+
+            Assert.IsTrue(num.AsSpan().TryParseInto(out sbyte value));
+            Assert.AreEqual(value, numRaw);
+        }
+
+        [Test]
+        public void Test_TryParseUInt8()
+        {
+            byte numRaw = 123;
+            var num = numRaw.ToString();
+
+            Assert.IsTrue(num.AsSpan().TryParseInto(out byte value));
+            Assert.AreEqual(value, numRaw);
+        }
+
+        [Test]
+        public void Test_TryParseInt16()
+        {
+            short numRaw = 12345;
+            var num = numRaw.ToString();
+
+            Assert.IsTrue(num.AsSpan().TryParseInto(out short value));
+            Assert.AreEqual(value, numRaw);
+        }
+
+        [Test]
+        public void Test_TryParseUInt16()
+        {
+            ushort numRaw = 12345;
+            var num = numRaw.ToString();
+
+            Assert.IsTrue(num.AsSpan().TryParseInto(out ushort value));
+            Assert.AreEqual(value, numRaw);
+        }
+
+        [Test]
         public void Test_TryParseInt32()
         {
-            var num = "123456".AsSpan();
+            var numRaw = 123456;
+            var num = numRaw.ToString();
 
-            Assert.IsTrue(num.TryParseInto(out int value));
-            Assert.AreEqual(value, 123456);
+            Assert.IsTrue(num.AsSpan().TryParseInto(out int value));
+            Assert.AreEqual(value, numRaw);
         }
+
+        [Test]
+        public void Test_TryParseUInt32()
+        {
+            var numRaw = 123456u;
+            var num = numRaw.ToString();
+
+            Assert.IsTrue(num.AsSpan().TryParseInto(out uint value));
+            Assert.AreEqual(value, numRaw);
+        }
+
+        [Test]
+        public void Test_TryParseInt64()
+        {
+            var numRaw = 123456l;
+            var num = numRaw.ToString();
+
+            Assert.IsTrue(num.AsSpan().TryParseInto(out long value));
+            Assert.AreEqual(value, numRaw);
+        }
+
+        [Test]
+        public void Test_TryParseUInt64()
+        {
+            var numRaw = 123456ul;
+            var num = numRaw.ToString();
+
+            Assert.IsTrue(num.AsSpan().TryParseInto(out ulong value));
+            Assert.AreEqual(value, numRaw);
+        }
+
 
         [Test]
         public void Test_TryParseSingle()
