3be4671905fdf9966e64c2c45a9ac57136f8bb8b
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2020-10-22 22:46:06 +0300

    Simple tests

---
+++ Tests/TryFormatSanityChecks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Backports;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class TryFormatSanityChecks
+    {
+        [Test]
+        [TestCase(int.MaxValue)]
+        [TestCase(int.MinValue)]
+        [TestCase(0)]
+        [TestCase(42)]
+        public void Test_TryFormatInt32(int value)
+        {
+            Span<char> buff = stackalloc char[32];
+            var wasFormatted = value.TryFormat(buff, out var charsWritten);
+            Assert.IsTrue(wasFormatted);
+
+            var str = buff.Slice(0, charsWritten).ToString();
+            Assert.AreEqual(value.ToString(), str);
+            
+        }
+    }
+}
