8dee59532474f89f46d84cea94d57a792bd6223a
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2020-10-23 01:47:55 +0300

    Some tests

--- Tests/TryFormatSanityChecks.cs
+++ Tests/TryFormatSanityChecks.cs
@@ -1,26 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Backports;
 using NUnit.Framework;
 
 namespace Tests
 {
+    public class Int32TryFormatSource
+    {
+        public static IEnumerable<int> IntValues { get; } = new[] {int.MaxValue, int.MinValue, 0, 4, -4, 42};
+
+        public static IEnumerable<string> Formats { get; } = new[] {string.Empty, "X", "G", "E", "E8", "G10", "D5", "D05", "###.0###"};
+
+        
+
+        public static IEnumerable<TestCaseData> TryFormatData =>
+            from x in IntValues
+            from y in Formats
+            select new TestCaseData(x, y);
+    }
+
     [TestFixture]
     public class TryFormatSanityChecks
     {
         [Test]
-        [TestCase(int.MaxValue)]
-        [TestCase(int.MinValue)]
-        [TestCase(0)]
-        [TestCase(42)]
-        public void Test_TryFormatInt32(int value)
+        [TestCaseSource(typeof(Int32TryFormatSource), nameof(Int32TryFormatSource.TryFormatData))]
+        public void Test_TryFormatInt32_Fmt(int value, string format)
         {
             Span<char> buff = stackalloc char[32];
-            var wasFormatted = value.TryFormat(buff, out var charsWritten);
+            var wasFormatted = value.TryFormat<int>(buff, out var charsWritten, format.AsSpan());
             Assert.IsTrue(wasFormatted);
 
             var str = buff.Slice(0, charsWritten).ToString();
-            Assert.AreEqual(value.ToString(), str);
-            
+            Assert.AreEqual(value.ToString(format), str);
         }
     }
 }
