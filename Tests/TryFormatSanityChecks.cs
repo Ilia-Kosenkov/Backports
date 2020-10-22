071de0d0375cce01624176798ef16b02f34a1120
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2020-10-23 02:14:53 +0300

    Added tests for UInt32 formatting

--- Tests/TryFormatSanityChecks.cs
+++ Tests/TryFormatSanityChecks.cs
@@ -20,6 +20,20 @@ namespace Tests
             select new TestCaseData(x, y);
     }
 
+    public class UInt32TryFormatSource
+    {
+        public static IEnumerable<uint> IntValues { get; } = new uint[] { uint.MaxValue, uint.MinValue, 4, 10500, 42 };
+
+        public static IEnumerable<string> Formats { get; } = new[] { string.Empty, "C", "X", "G", "E", "E8", "G10", "D5", "D05", "###.0###" };
+
+
+
+        public static IEnumerable<TestCaseData> TryFormatData =>
+            from x in IntValues
+            from y in Formats
+            select new TestCaseData(x, y);
+    }
+
     [TestFixture]
     public class TryFormatSanityChecks
     {
@@ -29,6 +43,18 @@ namespace Tests
         {
             Span<char> buff = stackalloc char[32];
             var wasFormatted = value.TryFormat<int>(buff, out var charsWritten, format.AsSpan());
+            Assert.IsTrue(wasFormatted);
+
+            var str = buff.Slice(0, charsWritten).ToString();
+            Assert.AreEqual(value.ToString(format), str);
+        }
+
+        [Test]
+        [TestCaseSource(typeof(UInt32TryFormatSource), nameof(UInt32TryFormatSource.TryFormatData))]
+        public void Test_TryFormatUInt32_Fmt(uint value, string format)
+        {
+            Span<char> buff = stackalloc char[32];
+            var wasFormatted = value.TryFormat<uint>(buff, out var charsWritten, format.AsSpan());
             Assert.IsTrue(wasFormatted);
 
             var str = buff.Slice(0, charsWritten).ToString();
