bb87949ace0072b71a479c712b24eb851edfecce
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-11-09 01:08:58 +0200

    Testing Double/Decimal

--- Tests/TryFormatSanityChecks.cs
+++ Tests/TryFormatSanityChecks.cs
@@ -128,6 +128,125 @@ namespace Tests
         }
     }
 
+    public class DoubleTryFormatSource
+    {
+        public static IEnumerable<double> IntValues { get; } = new[] { double.MinValue, double.MaxValue, Math.PI, double.NaN, double.NegativeInfinity, double.PositiveInfinity, 4, 10500, 42 };
+
+        public static IEnumerable<string> Formats { get; } = new[] { "G3", "G6", "E", "E6", "###.0###" };
+
+
+
+        public static IEnumerable<TestCaseData> TryFormatData =>
+            from x in IntValues
+            from y in Formats
+            select new TestCaseData(x, y);
+
+        public static IEnumerable<TestCaseData> TryFormatSpecialCaseData
+        {
+            get
+            {
+                
+                const double pi = Math.PI;
+                yield return new TestCaseData(pi, "G", "3.141592653589793");
+                yield return new TestCaseData(pi, "G7", "3.141593");
+                yield return new TestCaseData(pi, "G16", "3.141592653589793");
+                yield return new TestCaseData(pi, "G30", "3.14159265358979311599796346854");
+                yield return new TestCaseData(pi, "C", "$3.14");
+                yield return new TestCaseData(pi, "C16", "$3.1415926535897931");
+                yield return new TestCaseData(pi, "E", "3.141593E+000");
+                yield return new TestCaseData(pi, "E7", "3.1415927E+000");
+                yield return new TestCaseData(pi, "E16", "3.1415926535897931E+000");
+                yield return new TestCaseData(pi, "E30", "3.141592653589793115997963468544E+000");
+                yield return new TestCaseData(pi, "F", "3.14");
+                yield return new TestCaseData(pi, "F7", "3.1415927");
+                yield return new TestCaseData(pi, "F16", "3.1415926535897931");
+                yield return new TestCaseData(pi, "F30", "3.141592653589793115997963468544");
+
+                yield return new TestCaseData(double.MinValue, "G", "-1.7976931348623157E+308");
+                yield return new TestCaseData(double.MinValue, "G7", "-1.797693E+308");
+                yield return new TestCaseData(double.MinValue, "G16", "-1.797693134862316E+308");
+                yield return new TestCaseData(double.MinValue, "G30", "-1.79769313486231570814527423732E+308");
+                yield return new TestCaseData(double.MinValue, "E", "-1.797693E+308");
+                yield return new TestCaseData(double.MinValue, "E7", "-1.7976931E+308");
+                yield return new TestCaseData(double.MinValue, "E16", "-1.7976931348623157E+308");
+                yield return new TestCaseData(double.MinValue, "E30", "-1.797693134862315708145274237317E+308");
+
+                yield return new TestCaseData(double.MaxValue, "G", "1.7976931348623157E+308");
+                yield return new TestCaseData(double.MaxValue, "G7", "1.797693E+308");
+                yield return new TestCaseData(double.MaxValue, "G16", "1.797693134862316E+308");
+                yield return new TestCaseData(double.MaxValue, "G30", "1.79769313486231570814527423732E+308");
+                yield return new TestCaseData(double.MaxValue, "E", "1.797693E+308");
+                yield return new TestCaseData(double.MaxValue, "E7", "1.7976931E+308");
+                yield return new TestCaseData(double.MaxValue, "E16", "1.7976931348623157E+308");
+                yield return new TestCaseData(double.MaxValue, "E30", "1.797693134862315708145274237317E+308");
+
+            }
+        }
+    }
+
+    public class DecimalTryFormatSource
+    {
+        public static IEnumerable<decimal> IntValues { get; } = new[] { decimal.MinValue, decimal.MaxValue, (decimal)Math.PI, decimal.Zero, decimal.One, -4, 10500, -42 };
+
+        public static IEnumerable<string> Formats { get; } = new[] { "G3", "G6", "E", "E6", "F", "F6", "F10", "###.0###" };
+
+
+
+        public static IEnumerable<TestCaseData> TryFormatData =>
+            from x in IntValues
+            from y in Formats
+            select new TestCaseData(x, y);
+
+        public static IEnumerable<TestCaseData> TryFormatSpecialCaseData
+        {
+            get
+            {
+
+                const decimal pi = (decimal)Math.PI;
+                yield return new TestCaseData(pi, "G", "3.14159265358979");
+                yield return new TestCaseData(pi, "G7", "3.141593");
+                yield return new TestCaseData(pi, "G16", "3.14159265358979");
+                yield return new TestCaseData(pi, "C", "$3.14");
+                yield return new TestCaseData(pi, "C7", "$3.1415927");
+                yield return new TestCaseData(pi, "C16", "$3.1415926535897900");
+                yield return new TestCaseData(pi, "E", "3.141593E+000");
+                yield return new TestCaseData(pi, "E7", "3.1415927E+000");
+                yield return new TestCaseData(pi, "E16", "3.1415926535897900E+000");
+                yield return new TestCaseData(pi, "F", "3.14");
+                yield return new TestCaseData(pi, "F7", "3.1415927");
+                yield return new TestCaseData(pi, "F16", "3.1415926535897900");
+
+
+                yield return new TestCaseData(decimal.MaxValue, "G", "79228162514264337593543950335");
+                yield return new TestCaseData(decimal.MaxValue, "G7", "7.922816E+28");
+                yield return new TestCaseData(decimal.MaxValue, "G16", "7.922816251426434E+28");
+                yield return new TestCaseData(decimal.MaxValue, "C", "$79,228,162,514,264,337,593,543,950,335.00");
+                yield return new TestCaseData(decimal.MaxValue, "C7", "$79,228,162,514,264,337,593,543,950,335.0000000");
+                yield return new TestCaseData(decimal.MaxValue, "C16", "$79,228,162,514,264,337,593,543,950,335.0000000000000000");
+                yield return new TestCaseData(decimal.MaxValue, "E", "7.922816E+028");
+                yield return new TestCaseData(decimal.MaxValue, "E7", "7.9228163E+028");
+                yield return new TestCaseData(decimal.MaxValue, "E16", "7.9228162514264338E+028");
+                yield return new TestCaseData(decimal.MaxValue, "F", "79228162514264337593543950335.00");
+                yield return new TestCaseData(decimal.MaxValue, "F7", "79228162514264337593543950335.0000000");
+                yield return new TestCaseData(decimal.MaxValue, "F16", "79228162514264337593543950335.0000000000000000");
+
+                yield return new TestCaseData(decimal.MinValue, "G", "-79228162514264337593543950335");
+                yield return new TestCaseData(decimal.MinValue, "G7", "-7.922816E+28");
+                yield return new TestCaseData(decimal.MinValue, "G16", "-7.922816251426434E+28");
+                yield return new TestCaseData(decimal.MinValue, "C", "($79,228,162,514,264,337,593,543,950,335.00)");
+                yield return new TestCaseData(decimal.MinValue, "C7", "($79,228,162,514,264,337,593,543,950,335.0000000)");
+                yield return new TestCaseData(decimal.MinValue, "C16", "($79,228,162,514,264,337,593,543,950,335.0000000000000000)");
+                yield return new TestCaseData(decimal.MinValue, "E", "-7.922816E+028");
+                yield return new TestCaseData(decimal.MinValue, "E7", "-7.9228163E+028");
+                yield return new TestCaseData(decimal.MinValue, "E16", "-7.9228162514264338E+028");
+                yield return new TestCaseData(decimal.MinValue, "F", "-79228162514264337593543950335.00");
+                yield return new TestCaseData(decimal.MinValue, "F7", "-79228162514264337593543950335.0000000");
+                yield return new TestCaseData(decimal.MinValue, "F16", "-79228162514264337593543950335.0000000000000000");
+
+            }
+        }
+    }
+
     [TestFixture]
     public class TryFormatSanityChecks
     {
@@ -208,5 +327,57 @@ namespace Tests
             var str = buff.Slice(0, charsWritten).ToString();
             Assert.AreEqual(expected, str);
         }
+
+
+        [Test]
+        [TestCaseSource(typeof(DoubleTryFormatSource), nameof(DoubleTryFormatSource.TryFormatData))]
+        public void Test_TryFormatDouble_Fmt(double value, string format)
+        {
+            Span<char> buff = stackalloc char[384];
+            var wasFormatted = value.TryFormat<double>(buff, out var charsWritten, format.AsSpan());
+            Assert.IsTrue(wasFormatted);
+
+            var str = buff.Slice(0, charsWritten).ToString();
+            var expStr = value.ToString(format);
+            Assert.AreEqual(expStr, str);
+        }
+
+        [Test]
+        [TestCaseSource(typeof(DoubleTryFormatSource), nameof(DoubleTryFormatSource.TryFormatSpecialCaseData))]
+        public void Test_TryFormatDouble_Fmt(double value, string format, string expected)
+        {
+            Span<char> buff = stackalloc char[128];
+            var wasFormatted = value.TryFormat<double>(buff, out var charsWritten, format.AsSpan());
+            Assert.IsTrue(wasFormatted);
+
+            var str = buff.Slice(0, charsWritten).ToString();
+            Assert.AreEqual(expected, str);
+        }
+
+        [Test]
+        [TestCaseSource(typeof(DecimalTryFormatSource), nameof(DecimalTryFormatSource.TryFormatData))]
+        public void Test_TryFormatDecimal_Fmt(decimal value, string format)
+        {
+            Span<char> buff = stackalloc char[384];
+            var wasFormatted = value.TryFormat<decimal>(buff, out var charsWritten, format.AsSpan());
+            Assert.IsTrue(wasFormatted);
+
+            var str = buff.Slice(0, charsWritten).ToString();
+            var expStr = value.ToString(format);
+            Assert.AreEqual(expStr, str);
+        }
+
+        [Test]
+        [TestCaseSource(typeof(DecimalTryFormatSource), nameof(DecimalTryFormatSource.TryFormatSpecialCaseData))]
+        public void Test_TryFormatDecimal_Fmt(decimal value, string format, string expected)
+        {
+            Span<char> buff = stackalloc char[128];
+            var wasFormatted = value.TryFormat<decimal>(buff, out var charsWritten, format.AsSpan());
+            Assert.IsTrue(wasFormatted);
+
+            var str = buff.Slice(0, charsWritten).ToString();
+            Assert.AreEqual(expected, str);
+        }
+
     }
 }
