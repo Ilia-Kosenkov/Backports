ae3ecfc0230e1334bbeb4764b603bbaad325b963
Ilia <ilia.kosenkov.at.gm@gmail.com>
2021-01-11 13:57:38 +0200

    Testing first formats

--- Tests/TryFormatDateTimes.cs
+++ Tests/TryFormatDateTimes.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,17 +9,45 @@ using NUnit.Framework;
 
 namespace Tests
 {
+    public class TryFormatDateTimesProvider
+    {
+        public static IEnumerable<DateTime> Dates
+        {
+            get
+            {
+                yield return DateTime.Now;
+                yield return DateTime.UtcNow;
+                yield return DateTime.Today;
+            }
+        }
+
+        public static IEnumerable<string> Formats
+        {
+            get
+            {
+                yield return "O";
+                yield return "R";
+            }
+        }
+
+        public static IEnumerable DateTime_TestCaseData =>
+            from x in Dates
+            from y in Formats
+            select new TestCaseData(x, y);
+
+    }
+
     [TestFixture]
     public class TryFormatDateTimes
     {
+        
         [Test]
-        public void Test_SimpleDateTime()
+        [TestCaseSource(typeof(TryFormatDateTimesProvider), nameof(TryFormatDateTimesProvider.DateTime_TestCaseData))]
+        public void Test_DateTime(DateTime input, string format)
         {
             Span<char> buff = stackalloc char[128];
-            var now = DateTime.Now;
-
-            Assert.IsTrue(now.TryFormat(buff, out var nChar, "O".AsSpan()));
-            Assert.AreEqual(buff.Slice(0, nChar).ToString(), now.ToString("O"));
+            Assert.IsTrue(input.TryFormat(buff, out var nChars, format.AsSpan()));
+            Assert.AreEqual(buff.Slice(0, nChars).ToString(), input.ToString(format));
         }
     }
 }
